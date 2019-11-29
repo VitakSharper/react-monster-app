0ee5faead87a8473ef9fb3d963abe8bf4fcf4371
VitakSharper <v.banard@gmail.com>
2019-11-29 20:47:58 +0100

    Add some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import CardList from "./components/card-list/Card-list.component";
+import CardList from "./components/card-list/Card-list";
 
 class App extends React.Component {
     state = {
         url: 'https://jsonplaceholder.typicode.com/users',
+        searchField: '',
         monsters: []
     };
 
@@ -17,6 +18,8 @@ class App extends React.Component {
     render() {
         return (
             <div className='App'>
+                <input type='text' placeholder='search monsters'
+                       onChange={e => this.setState({searchField: e.target.value})}/>
                 <CardList
                     monsters={this.state.monsters}/>
             </div>
